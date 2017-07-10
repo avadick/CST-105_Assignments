e3e3b7818f58b301ac992d42439cbe923c1370f1
Ava Dick <avadick@me.com>
2017-07-09 17:51:13 -0700

    no message

---
+++ Topic 1/Introduction.java
@@ -0,0 +1,8 @@
+public class Introduction {
+	public static void main(String[] args) {
+		// display messages: My name is Ava, My favorite color is blue, and If I won the lottery... on console
+		System.out.println("My name is Ava");
+		System.out.println("My favorite color is blue");
+		System.out.println("If I won the lottery, I would invest half and donate the rest");
+	}
+}
